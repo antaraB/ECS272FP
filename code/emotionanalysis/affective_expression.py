9115618ef3fa6b54cf440e92acdbdf6f1a580f6f
Antara Bhowmick <antara.7695@gmail.com>
2018-03-09 16:40:22 -0800

    Updated file

--- code/emotionanalysis/affective_expression.py
+++ code/emotionanalysis/affective_expression.py
@@ -20,84 +20,6 @@ def match (word, lexicon, stemmer):
 			val.append(line)
 	return val
 
-if __name__=="__main__":
-	# for testing
-	filename="data/pickle/@BarackObama_tweets_dict.p"
-	#filename=''
-
-	parser=argparse.ArgumentParser()
-	parser.add_argument('filename',action='store', help="Complete path of pickled (preprocessed tweets) file")
-	parser.add_argument('-s','--stemmer', choices=['snowball','porter'], , default='snowball', help=" Select stemmer to be used. Choices: snowball or porter")
-	args=parser.parse_args()
-
-	if not filename:
-		filename=args.filename
-
-	'''
-	add support for cmd line stemmer
-	'''
-
-	# dictionary storing all values
-	tweetdata=pickle.load(open(filename))
-
-	nrcfile="lexicon/nrc/newnrc.csv"
-	with open (nrcfile) as f:
-		nrclexicon=list(csv.reader(f))
-
-	anewfile="lexicon/anew/newanew2010.csv"
-	with open (anewfile) as f:
-		anewlexicon=list(csv.reader(f))
-
-	ps = PorterStemmer()
-	sb = SnowballStemmer("english")
-	lem = WordNetLemmatizer()
-
-
-	affexp={}
-	# plutchik
-	affexp=affexpdetection(tweetdata, affexp, 'plutchik', 8, nrclexicon, 1)
-
-		# affexp['tweetID']['word']={vad':[v,a,d], 'plutchik'=[anger,anticipation,disgust,fear,joy,sadness,surprise,trust]}
-'''	for tweetid in tweetdata:
-		affexp[tweetid]={}
-		pval=[0.0]*8
-		# vad=[0]*3
-		for word in tweetdata[tweetid]['words']:
-			res=match(word, nrclexicon, '')
-			# exact match
-			if len(res)==1:
-				pval=[x+int(y) for x,y in zip(pval,res[0][1:])]
-				print "exact "+word
-			# no exact match -> check using lemmatized word
-			elif not res:
-				res=match(lem.lemmatize(word),nrclexicon,'')
-				if len(res)==1:
-					pval=[x+int(y) for x,y in zip(pval,res[0][1:])]
-					print "lemmatize " +word
-				# no exact match -> check using stemmer
-				else :
-					res=match(word, nrclexicon, sb)
-					if len(res)==1:
-						pval=[x+int(y) for x,y in zip(pval,res[0][1:])]
-						print "stop "+word
-					# multiple matches
-					elif len(res)>1:
-						# check if all have same plutchik categories
-						# MAKESHIFT: just average out
-						resmultiple=[0]*8 #int, so will only surface if present in both
-						for option in res:
-							resmultiple=[x+int(y) for x,y in zip(resmultiple,option[1:])]
-						resmultiple=[x/len(res) for x in resmultiple]
-						pval=[x+y for x,y in zip(pval,resmultiple)]
-						print word,res,pval
-					# else : no matches
-					# check with anew? Or wordnet?
-		pcount=sum(pval)
-		if pcount:
-			pval=[x/pcount for x in pval]
-			affexp[tweetid]['plutchik']=pval'''
-
-# affexp['tweetID']['word']={vad':[v, a, d], 'plutchik'=[anger, anticipation, disgust, fear, joy, sadness, surprise, trust]}
 def affexpdetection(tweetdata, affexp, modeltype, modelcount, lexicon, to_print):
 	if to_print:
 		count=0
@@ -143,10 +65,95 @@ def affexpdetection(tweetdata, affexp, modeltype, modelcount, lexicon, to_print)
 							print "Multiple values for {}:{}: FINAL:{}".format(word,res,resmultiple)
 					# else : no matches
 					# check with anew? Or wordnet?
+				if modeltype=='plutchik':
+					values=[int(x) for x in values]
 				if word not in affexp[tweetid]:
 					affexp[tweetid][word]={}
 				affexp[tweetid][word][modeltype]=values
-		return affexp
+	return affexp
+
+if __name__=="__main__":
+	# for testing
+	# filename="data/pickle/@BarackObama_tweets_dict.p"
+	filename=''
+
+	parser=argparse.ArgumentParser()
+	parser.add_argument('filename',action='store', help="Complete path of pickled (preprocessed tweets) file")
+	parser.add_argument('-s','--stemmer', choices=['snowball','porter'], , default='snowball', help=" Select stemmer to be used. Choices: snowball or porter")
+	args=parser.parse_args()
+
+	if not filename:
+		filename=args.filename
+
+
+	'''
+	add support for cmd line stemmer
+	'''
+
+	# dictionary storing all values
+	tweetdata=pickle.load(open(filename))
+
+	nrcfile="lexicon/nrc/newnrc.csv"
+	with open (nrcfile) as f:
+		nrclexicon=list(csv.reader(f))
+
+	anewfile="lexicon/anew/newanew2010.csv"
+	with open (anewfile) as f:
+		anewlexicon=list(csv.reader(f))
+
+	ps = PorterStemmer()
+	sb = SnowballStemmer("english")
+	lem = WordNetLemmatizer()
+
+
+	affexp={}
+	# plutchik
+	affexp=affexpdetection(tweetdata, affexp, 'plutchik', 8, nrclexicon, 1)
+	# vad
+	affexp=affexpdetection(tweetdata, affexp, 'vad', 3, anewlexicon, 1)
+
+		# affexp['tweetID']['word']={vad':[v,a,d], 'plutchik'=[anger,anticipation,disgust,fear,joy,sadness,surprise,trust]}
+'''	for tweetid in tweetdata:
+		affexp[tweetid]={}
+		pval=[0.0]*8
+		# vad=[0]*3
+		for word in tweetdata[tweetid]['words']:
+			res=match(word, nrclexicon, '')
+			# exact match
+			if len(res)==1:
+				pval=[x+int(y) for x,y in zip(pval,res[0][1:])]
+				print "exact "+word
+			# no exact match -> check using lemmatized word
+			elif not res:
+				res=match(lem.lemmatize(word),nrclexicon,'')
+				if len(res)==1:
+					pval=[x+int(y) for x,y in zip(pval,res[0][1:])]
+					print "lemmatize " +word
+				# no exact match -> check using stemmer
+				else :
+					res=match(word, nrclexicon, sb)
+					if len(res)==1:
+						pval=[x+int(y) for x,y in zip(pval,res[0][1:])]
+						print "stop "+word
+					# multiple matches
+					elif len(res)>1:
+						# check if all have same plutchik categories
+						# MAKESHIFT: just average out
+						resmultiple=[0]*8 #int, so will only surface if present in both
+						for option in res:
+							resmultiple=[x+int(y) for x,y in zip(resmultiple,option[1:])]
+						resmultiple=[x/len(res) for x in resmultiple]
+						pval=[x+y for x,y in zip(pval,resmultiple)]
+						print word,res,pval
+					# else : no matches
+					# check with anew? Or wordnet?
+		pcount=sum(pval)
+		if pcount:
+			pval=[x/pcount for x in pval]
+			affexp[tweetid]['plutchik']=pval'''
+
+# affexp['tweetID']['word']={vad':[v, a, d], 'plutchik'=[anger, anticipation, disgust, fear, joy, sadness, surprise, trust]}
+
 
 
 '''
