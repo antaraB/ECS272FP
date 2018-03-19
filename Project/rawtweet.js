c9e02af85fe0f97b685eb451e4eeb240428dde05
preritauti <preritauti@gmail.com>
2018-03-18 20:58:39 -0700

    Updated visualizations

---
+++ Project/rawtweet.js
@@ -0,0 +1,38 @@
+var data3 = [
+    {"tweet":"This is one tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is next tweet"},
+    {"tweet":"This is last tweet"}
+];
+
+
+  var  i, txt = "";
+  
+      for (i in data3) {
+        console.log("Data", data3[i].tweet);
+        txt += "<div class=\"textbox\" border='1'>"
+        txt += "<p>" + data3[i].tweet + "</p>";
+        txt += "<p class=\"timep\">" + "Date of the tweet" + "</p>";
+        txt += "</div>"   
+      }
+      document.getElementById("tweets_box").innerHTML = txt;
